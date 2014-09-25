ae6285a1641490a58093b6aed2c8712315879e67
Chris Roberson <chrisronline@gmail.com>
2014-09-25 12:00:54 -0400

    Some style fixes

--- web/app/components/_constants/dateStrings.constant.js
+++ web/app/components/_constants/dateStrings.constant.js
@@ -7,6 +7,7 @@
     DATE_SHORTHAND: 'MM/DD/YYYY',
     DATE_LONG: 'dddd, MMMM Do YYYY',
     DATE_CONTROL: 'YYYY-MM-DD',
+    DATE_AND_TIME: 'MM/DD/YYYY h:mm a',
     TIME_SHORTHAND: 'h:mm a',
     TIME_CONTROL: 'HH:mm'
   };
